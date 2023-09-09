8b3b46a45806afcbfc667db43c906532291a5fda
Fred T. Dunaway <fred.t.dunaway@gmail.com>
2023-09-09 10:55:35 -0400

    Added command line params

--- mobileNetImageProcessing.py
+++ mobileNetImageProcessing.py
@@ -6,12 +6,59 @@ import imageio.v2 as imageio
 import PIL.Image as Image
 import glob
 from pathlib import Path
+import os
+import argparse
 
-#TODO:  What does mobile net think image 5MTC0147.JPG is?'
+#TODO: Add ability to attempt to 'read' tail number or licence plate.
 
-imageDir = 'cameraImages'
-canidateDir = imageDir + '/airplaneCanidates/'
-carCanidateDir = imageDir + '/carCanidates/'
+parser = argparse.ArgumentParser(description='AI image classifier for finding airplane images and cars.')
+parser.add_argument('--imageDir', 
+                    required=True,
+                    default='cameraImages',
+                    help='The directory with the images to process')
+parser.add_argument('--baseDir',
+                    default='.',
+                    help='The base directory where filtered images will be placed.')
+parser.add_argument('--airplaneCanidateDir',
+                    default='airplaneImages',
+                    help='The directory from the base directory where airplane images will be placed.')
+parser.add_argument('--carDir',
+                    default='carCanidateImages',
+                    help='The directory from the base directory where car images will be placed')
+
+args = parser.parse_args()
+
+imageDir = args.imageDir
+canidateDir = args.baseDir + '/' + imageDir + '/' + args.airplaneCanidateDir + '/'
+carCanidateDir = args.baseDir + '/' + imageDir + '/' + args.carDir + '/'
+
+if not Path.exists(imageDir):
+    print('Base image dir {} does not exisit.  Exiting'.format(imageDir))
+    exit -1
+    
+if not Path.exists(args.baseDir):
+    print('Base dir of {} does not exisit. Creating...'.format(args.baseDir))
+    try:
+        Path.mkdir(args.baseDir)
+    except Exception as e:
+        print('Unable to create base dir of {}'.format(args.baseDir))
+        exit -1
+if not Path.exists(canidateDir):
+    print('Airplane canidate dir {} missing.  Creating...'.format(canidateDir))
+    Path.mkdir(canidateDir)
+
+if not Path.exists(carCanidateDir):
+    print('Car canidate dir {} missing.  Creating...'.format(carCanidateDir))
+    Path.mkdir(carCanidateDir)
+    
+planeImageCount = len(os.listdir(canidateDir))
+carImageCount = len(os.listdir(carCanidateDir))
+if (planeImageCount != 0):
+    print('Warning: Canidate plane images exisit in dir {}!'.format(canidateDir))
+
+if (carImageCount != 0):
+    print('Warning:  Car canidate images exisit in directory {}!'.format(carCanidateDir))
+
 canidateLabels = ['white_stork', 'warplane', 'space_shuttle', 'wing', 'airliner']
 carLabels = ['minivan', 'jeep']
 img_height = 224
